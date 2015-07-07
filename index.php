dbd0ff1a7509a57312b3c3f4f9fab378212570ff
hugal <hugal@example.com>
2015-07-07 17:32:53 +0200

    commit 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+//use ecommerce\controller\Controller;
+//
+//require 'inc/conf.inc.php';
+//new Controller();
+//
+//
+//
+
+include "inc/conf.inc.php";
+
+include "src/View/home.php";
